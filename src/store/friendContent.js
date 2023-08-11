ed36f69f1986f3e129b4af3129139aeebebe18e3
YS_workPC <ong_ys86@yahoo.com.sg>
2023-08-11 22:14:49 +0800

    add storing info in localstorage

--- src/store/friendContent.js
+++ src/store/friendContent.js
@@ -16,63 +16,77 @@ const intialFriends = [
   },
   {
     friendName: "friend3",
-    paidInfo: [
-      { place: "cafe", paidAmt: "9", comment: "taxi", exclude: [] },
-    ],
+    paidInfo: [{ place: "cafe", paidAmt: "9", comment: "taxi", exclude: [] }],
   },
 ];
 
+const mem =
+  localStorage.getItem("friends") !== null
+    ? JSON.parse(localStorage.getItem("friends"))
+    : intialFriends;
+
 const friendsSlice = createSlice({
   name: "friendContent",
-  initialState: intialFriends,
+  initialState: mem,
   reducers: {
-    resetFriends(state){
-      return [
+    resetFriends(state) {
+      const resetState = [
         {
-          friendName: "friend1" ,
+          friendName: "friend1",
           paidInfo: [{ place: "", paidAmt: 0, comment: "", exclude: [] }],
         },
-      ]
+      ];
+      localStorage.setItem("friends", JSON.stringify(resetState));
+      return resetState;
     },
     addFriend(state) {
-      return [
+      const addState = [
         ...state,
         {
           friendName: "friend" + (state.length + 1),
           paidInfo: [{ place: "", paidAmt: 0, comment: "", exclude: [] }],
         },
       ];
+      localStorage.setItem("friends", JSON.stringify(addState));
+      return addState;
     },
     removeFriend(state, action) {
       state.splice(action.payload.ind, 1);
       state.map((f) => f.paidInfo.map((pInfo) => (pInfo.exclude = [])));
+      localStorage.setItem("friends", JSON.stringify(state));
     },
     setFriendName(state, action) {
       state[action.payload.ind].friendName = action.payload.change;
+      localStorage.setItem("friends", JSON.stringify(state));
     },
     addPaidDetail(state, action) {
       state[action.payload.ind].paidInfo = [
         ...state[action.payload.ind].paidInfo,
         { place: "", paidAmt: 0, comment: "", exclude: [] },
       ];
+      localStorage.setItem("friends", JSON.stringify(state));
     },
     removePaidDetail(state, action) {
       state[action.payload.ind].paidInfo.splice(action.payload.ind1, 1);
+      localStorage.setItem("friends", JSON.stringify(state));
     },
     setPaidDetailPlace(state, action) {
       const friendX = state[action.payload.ind];
       const paidInfoX = friendX.paidInfo[action.payload.ind1];
       paidInfoX.place = action.payload.place;
+      localStorage.setItem("friends", JSON.stringify(state));
     },
     setPaidDetailAmt(state, action) {
       const friendX = state[action.payload.ind];
       const paidInfoX = friendX.paidInfo[action.payload.ind1];
       paidInfoX.paidAmt = action.payload.paidAmt;
+      localStorage.setItem("friends", JSON.stringify(state));
     },
     setPaidDetailComment(state, action) {
       const friendX = state[action.payload.ind];
       const paidInfoX = friendX.paidInfo[action.payload.ind1];
       paidInfoX.comment = action.payload.comment;
+      localStorage.setItem("friends", JSON.stringify(state));
     },
     addPaidDetailExclude(state, action) {
       const friendX = state[action.payload.ind];
@@ -80,6 +94,7 @@ const friendsSlice = createSlice({
       const set = new Set(paidInfoX.exclude);
       set.add(action.payload.f);
       paidInfoX.exclude = [...set].toSorted();
+      localStorage.setItem("friends", JSON.stringify(state));
     },
     removePaidDetailExclude(state, action) {
       const friendX = state[action.payload.ind];
@@ -87,6 +102,7 @@ const friendsSlice = createSlice({
       paidInfoX.exclude = paidInfoX.exclude.filter(
         (x) => x !== action.payload.f
       );
+      localStorage.setItem("friends", JSON.stringify(state));
     },
   },
 });
